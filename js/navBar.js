d8bec9329f3bb51ab1c0503390c159db28b8de8c
Khizar Tariq <khizar.tariqq2001@gmail.com>
2023-06-25 19:34:14 -0400

    Adjusted scrolling animation to be dependent on the skills div instead of the individual images. This way the animation appears once 75% of the skills div is visible, so both rows are showing. This prevents a delay if the user does not reveal both rows at the same time (scrolling slowly).

--- js/navBar.js
+++ js/navBar.js
@@ -1,21 +1,24 @@
 var menuVisible = false;
-
 var prevScrollpos = window.pageYOffset;
+
 window.onscroll = function(){
 	var currentScrollPos = window.pageYOffset;
+	
 	if (prevScrollpos > currentScrollPos){
 		document.getElementById("navbar").style.top = "0";
 	}
+	
 	else{
 		document.getElementById("navbar").style.top = "-80px";
 		
+		// If user scrolls down while menu is open then close it
 		if(menuVisible){
 			mMenu();
 		}
 	}
+	
 	prevScrollpos = currentScrollPos;
 }
-
 
 const menu = document.querySelector('#mobile-menu');
 const links = document.querySelector('.navbarMenu');
@@ -25,10 +28,12 @@ const mMenu = () => {
   menuVisible = links.classList.toggle('active');
 }
 
+// Close menu when user presses the X
 menu.addEventListener('click', mMenu);
 
 const listItems = document.getElementsByClassName('navbarItem');
 
+// Close the menu once an option is selected
 for (let i=0; i < listItems.length; i++){
 	listItems[i].addEventListener('click',mMenu);
 }
--- js/scrollAnimations.js
+++ js/scrollAnimations.js
@@ -1,95 +1,24 @@
 const listImgs = document.getElementsByClassName('skillImage');
+const skillsDiv = document.getElementById('skills');
 
 const callback = (entries, observer) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      entry.target.classList.add("fadeIn")
+      listImgs[0].classList.add("fadeIn")
+	  listImgs[1].classList.add("fadeIn2")
+	  listImgs[2].classList.add("fadeIn3")
+	  listImgs[3].classList.add("fadeIn4")
+	  listImgs[4].classList.add("fadeIn5")
+	  listImgs[5].classList.add("fadeIn6")
+	  listImgs[6].classList.add("fadeIn7")
+	  listImgs[7].classList.add("fadeIn8")
+	  listImgs[8].classList.add("fadeIn9")
     }
   })
 }
 
-const callback2 = (entries, observer) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("fadeIn2")
-    }
-  })
-}
-
-const callback3 = (entries, observer) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("fadeIn3")
-    }
-  })
-}
-
-const callback4 = (entries, observer) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("fadeIn4")
-    }
-  })
-}
-
-const callback5 = (entries, observer) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("fadeIn5")
-    }
-  })
-}
-
-const callback6 = (entries, observer) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("fadeIn6")
-    }
-  })
-}
-
-const callback7 = (entries, observer) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("fadeIn7")
-    }
-  })
-}
-
-const callback8 = (entries, observer) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("fadeIn8")
-    }
-  })
-}
-
-const callback9 = (entries, observer) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("fadeIn9")
-    }
-  })
-}
-
-const options = {}
+// Start the animation once 90% of the skills div is visible
+const options = {threshold: "0.75"}
 
 const myObserver = new IntersectionObserver(callback, options)
-const myObserver2 = new IntersectionObserver(callback2, options)
-const myObserver3 = new IntersectionObserver(callback3, options)
-const myObserver4 = new IntersectionObserver(callback4, options)
-const myObserver5 = new IntersectionObserver(callback5, options)
-const myObserver6 = new IntersectionObserver(callback6, options)
-const myObserver7 = new IntersectionObserver(callback7, options)
-const myObserver8 = new IntersectionObserver(callback8, options)
-const myObserver9 = new IntersectionObserver(callback9, options)
-
-myObserver.observe(listImgs[0])
-myObserver2.observe(listImgs[1])
-myObserver3.observe(listImgs[2])
-myObserver4.observe(listImgs[3])
-myObserver5.observe(listImgs[4])
-myObserver6.observe(listImgs[5])
-myObserver7.observe(listImgs[6])
-myObserver8.observe(listImgs[7])
-myObserver9.observe(listImgs[8])
+myObserver.observe(skillsDiv)
