3f2de8d2ab2cb8389f70f2411222f6be9ad63b4b
unknown <khizar.tariqq2001@gmail.com>
2025-01-09 10:49:49 -0500

    Updated skillset and resume

--- js/scrollAnimations.js
+++ js/scrollAnimations.js
@@ -4,21 +4,28 @@ const skillsDiv = document.getElementById('skills');
 const callback = (entries, observer) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      listImgs[0].classList.add("fadeIn")
-	  listImgs[1].classList.add("fadeIn2")
-	  listImgs[2].classList.add("fadeIn3")
-	  listImgs[3].classList.add("fadeIn4")
-	  listImgs[4].classList.add("fadeIn5")
-	  listImgs[5].classList.add("fadeIn6")
-	  listImgs[6].classList.add("fadeIn7")
-	  listImgs[7].classList.add("fadeIn8")
-	  listImgs[8].classList.add("fadeIn9")
+		listImgs[0].classList.add("fadeIn")
+		listImgs[1].classList.add("fadeIn2")
+		listImgs[2].classList.add("fadeIn3")
+		listImgs[3].classList.add("fadeIn4")
+		listImgs[4].classList.add("fadeIn5")
+		listImgs[5].classList.add("fadeIn6")
+		listImgs[6].classList.add("fadeIn7")
+		listImgs[7].classList.add("fadeIn8")
+		listImgs[8].classList.add("fadeIn9")
+		listImgs[9].classList.add("fadeIn10")
+		listImgs[10].classList.add("fadeIn11")
+		listImgs[11].classList.add("fadeIn12")
+		listImgs[12].classList.add("fadeIn13")
+		listImgs[13].classList.add("fadeIn14")
+		listImgs[14].classList.add("fadeIn15")
+		listImgs[15].classList.add("fadeIn16")
     }
   })
 }
 
-// Start the animation once 90% of the skills div is visible
-const options = {threshold: "0.75"}
+// Start the animation once 50% of the skills div is visible
+const options = {threshold: "0.5"}
 
 const myObserver = new IntersectionObserver(callback, options)
 myObserver.observe(skillsDiv)
